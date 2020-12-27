ccf014d798ed24cc8d77cb1579491bf99244839a
andreagwieland <andreagwieland@penguin>
2020-12-26 19:09:46 -0800

    figuring out env vars

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,7 @@ import express from "express"
 import path from "path"
 import http from "http"
 
-const port: number = process.env.PORT;
+const port:number = parseInt(process.env.PORT);
 
 class App {
     private server: http.Server
